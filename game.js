0812d3787c8199c82cef98b0093193a6fcc905f1
oscurantismo <lubyatowo@gmail.com>
2025-03-21 19:01:42 -0400

    Update game.js

--- game.js
+++ game.js
@@ -19,46 +19,73 @@ window.onload = () => {
     game = new Phaser.Game(gameConfig);
 };
 
-let trump, shoeCursor, score = 0, scoreText;
+let trump, shoeCursor, punches = 0, punchesText;
+let punchSounds = [];
+let trumpOriginalTexture = "trump";
+let trumpHitTexture = "trump_hit";
+let hitCooldown = false;
 
 function preload() {
     this.load.image("trump", "trump.png");
+    this.load.image("trump_hit", "trump_hit.png");
     this.load.image("shoe", "shoe.png");
+
+    // Load multiple punch sounds
+    for (let i = 1; i <= 4; i++) {
+        this.load.audio("punch" + i, `punch${i}.mp3`);
+    }
 }
 
 function create() {
-    // Add Trump in the centre with larger scale
-    trump = this.add.image(this.scale.width / 2, this.scale.height / 2, "trump").setScale(0.8);
+    // Add Trump image
+    trump = this.add.image(this.scale.width / 2, this.scale.height / 2, trumpOriginalTexture).setScale(0.8);
 
-    // Score text (top-left, scalable size)
-    scoreText = this.add.text(20, 20, "Score: 0", {
+    // Add score text
+    punchesText = this.add.text(20, 20, "Punches: 0", {
         fontSize: Math.round(this.scale.width * 0.05) + "px",
         fill: "#000"
     });
 
-    // Hide cursor
-    this.input.setDefaultCursor('none');
+    // Add punch sounds to array
+    for (let i = 1; i <= 4; i++) {
+        punchSounds.push(this.sound.add("punch" + i));
+    }
 
-    // Shoe as fake cursor
-    shoeCursor = this.add.image(0, 0, "shoe").setScale(0.5);
-    shoeCursor.setDepth(1);
+    // Custom cursor (shoe image)
+    this.input.setDefaultCursor("none");
+    shoeCursor = this.add.image(0, 0, "shoe").setScale(0.5).setDepth(1);
 }
 
 function update() {
     const pointer = this.input.activePointer;
 
-    // Follow mouse/finger
+    // Move shoe with pointer
     shoeCursor.x = pointer.x;
     shoeCursor.y = pointer.y;
 
-    // On click/tap
     if (pointer.isDown && !pointer.wasDown) {
         pointer.wasDown = true;
 
         const bounds = trump.getBounds();
         if (Phaser.Geom.Rectangle.Contains(bounds, pointer.x, pointer.y)) {
-            score++;
-            scoreText.setText("Score: " + score);
+            // Increase punch count
+            punches++;
+            punchesText.setText("Punches: " + punches);
+
+            // Play a random punch sound
+            const randomSound = Phaser.Math.RND.pick(punchSounds);
+            randomSound.play();
+
+            // Change Trump texture briefly
+            if (!hitCooldown) {
+                hitCooldown = true;
+                trump.setTexture(trumpHitTexture);
+
+                setTimeout(() => {
+                    trump.setTexture(trumpOriginalTexture);
+                    hitCooldown = false;
+                }, 200);
+            }
         }
     }
 
