18ea88bea9283cbe37b00d3373236bdd4d80a24a
oscurantismo <lubyatowo@gmail.com>
2025-03-21 23:44:19 -0400

    Update game.js

--- game.js
+++ game.js
@@ -29,7 +29,6 @@ let trumpHitTexture = "trump_hit";
 let hitCooldown = false;
 let soundEnabled = true;
 let soundButton;
-let pointerPreviouslyDown = false;
 
 function preload() {
     this.load.image("trump", "trump.png");
@@ -54,7 +53,8 @@ function create() {
 
     trump = this.add.image(this.scale.width / 2, this.scale.height / 2, trumpOriginalTexture)
         .setScale(trumpScale)
-        .setOrigin(0.5);
+        .setOrigin(0.5)
+        .setInteractive();
 
     punchesText = this.add.text(20, 20, "Punches: " + punches, {
         fontSize: Math.round(this.scale.width * 0.05) + "px",
@@ -65,8 +65,11 @@ function create() {
         punchSounds.push(this.sound.add("punch" + i));
     }
 
+    // Hide default cursor and show shoe sprite
     this.input.setDefaultCursor("none");
-    shoeCursor = this.add.image(0, 0, "shoe").setScale(0.5).setDepth(1);
+    shoeCursor = this.add.image(this.input.activePointer.x, this.input.activePointer.y, "shoe")
+        .setScale(0.5)
+        .setDepth(999);
 
     const iconSize = 50;
     soundButton = this.add.image(this.scale.width - iconSize / 2 - 20, iconSize / 2 + 20, "sound_on")
@@ -88,7 +91,7 @@ function create() {
     }).setInteractive();
 
     leaderboardButton.on("pointerdown", () => {
-        fetch("https://web-production-5454.up.railway.app/")
+        fetch("https://web-production-5454.up.railway.app/leaderboard")
             .then(res => res.json())
             .then(data => {
                 const text = data.map((entry, i) =>
@@ -99,44 +102,39 @@ function create() {
             })
             .catch(() => alert("Failed to load leaderboard."));
     });
+
+    // Add input event listener on trump
+    trump.on("pointerdown", () => {
+        punches++;
+        punchesText.setText("Punches: " + punches);
+        localStorage.setItem("punches", punches);
+
+        if (soundEnabled) {
+            const randomSound = Phaser.Math.RND.pick(punchSounds);
+            randomSound.play();
+        }
+
+        if (!hitCooldown) {
+            hitCooldown = true;
+            trump.setTexture(trumpHitTexture);
+            setTimeout(() => {
+                trump.setTexture(trumpOriginalTexture);
+                hitCooldown = false;
+            }, 200);
+        }
+
+        fetch("https://web-production-5454.up.railway.app/submit", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username: Telegram.WebApp.initDataUnsafe.user?.username || "Anonymous",
+                score: punches
+            })
+        });
+    });
 }
 
 function update() {
     const pointer = this.input.activePointer;
-    shoeCursor.x = pointer.x;
-    shoeCursor.y = pointer.y;
-
-    if (pointer.isDown && !pointerPreviouslyDown) {
-        const bounds = trump.getBounds();
-        if (Phaser.Geom.Rectangle.Contains(bounds, pointer.x, pointer.y)) {
-            punches++;
-            punchesText.setText("Punches: " + punches);
-            localStorage.setItem("punches", punches);
-
-            if (soundEnabled) {
-                const randomSound = Phaser.Math.RND.pick(punchSounds);
-                randomSound.play();
-            }
-
-            if (!hitCooldown) {
-                hitCooldown = true;
-                trump.setTexture(trumpHitTexture);
-                setTimeout(() => {
-                    trump.setTexture(trumpOriginalTexture);
-                    hitCooldown = false;
-                }, 200);
-            }
-
-            fetch("https://web-production-5454.up.railway.app/", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    username: Telegram.WebApp.initDataUnsafe.user?.username || "Anonymous",
-                    score: punches
-                })
-            });
-        }
-    }
-
-    pointerPreviouslyDown = pointer.isDown;
+    shoeCursor.setPosition(pointer.x, pointer.y);
 }
