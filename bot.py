418dfac71b07534ba44079afb78e6640db7efd0c
oscurantismo <lubyatowo@gmail.com>
2025-03-23 21:09:20 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ def main():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("status", status))
-    dp.add_handler(CallbackQueryHandler(game_callback))  # No pattern needed
+    dp.add_handler(CallbackQueryHandler(game_callback, pattern=f"^{GAME_SHORT_NAME}$"))
     dp.add_error_handler(error_handler)
 
     updater.start_polling()
